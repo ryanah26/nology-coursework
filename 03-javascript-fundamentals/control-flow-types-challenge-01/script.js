37554b4746abaac473ebd0016ce3698015671a72
Ryan Harrison <ryanah26@gmail.com>
2022-05-03 16:37:20 +0100

    weekend countdown made in script.js

--- 03-javascript-fundamentals/control-flow-types-challenge-01/script.js
+++ 03-javascript-fundamentals/control-flow-types-challenge-01/script.js
@@ -1,6 +1,34 @@
+//  Positive or Negative Checker
+
 const myNumber = 3;
-if (myNumber > 0) {
+if (myNumber >= 0) {
     console.log("My number is positive!")
 } else {
     console.log("My number is negative!")
+}
+
+//  Days until weekend notifier
+
+switch (new Date().getDay()) {
+    case 0:
+        console.log("It's already the weekend!")
+        break;
+    case 1:
+        console.log("4 days left")
+        break;
+    case 2:
+        console.log("3 days left")
+        break;
+    case 3:
+        console.log("2 days left")
+        break;
+    case 4:
+        console.log("1 day left")
+        break;
+    case 5:
+        console.log("0 days left")
+        break;
+    case 6:
+        console.log("It's already the weekend!")
+        break;
 }
