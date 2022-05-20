296cdf9de2df5db7027ffe3e3fe880e2e7aa3917
Ryan Harrison <ryanah26@gmail.com>
2022-05-20 16:29:51 +0100

    created function for number buttons

--- 03-javascript-fundamentals/js-calculator/script.js
+++ 03-javascript-fundamentals/js-calculator/script.js
@@ -1,9 +1,8 @@
-const updateDisplay = (number) => displayField.innerHTML = number;
 
 let primaryNumber = "";
 let operator = "";
 let secondaryNumber = "";
-let storeNumber = 0;
+let storedNumber = 0;
 let equals = 0;
 
 const acButton = document.querySelectorAll('.js-acBtn');
@@ -12,5 +11,26 @@ const numberButton = document.querySelectorAll('.js-numBtn');
 const decimalButton = document.querySelectorAll('.js-deciBtn');
 const equalsButton = document.querySelectorAll('.js-equalsBtn');
 const calculatorDisplay = document.querySelectorAll('.js-display');
+
+const updateDisplay = (number) => calculatorDisplay.innerHTML = number;
+
+numberButton.forEach(numBtn => {
+    numBtn.addEventListener('click', (event) => {
+        (!primaryNumber) ? primaryNumber = event.target.innerHTML : primaryNumber += event.target.innerHTML;
+    })
+})
+
+
+functionButton.forEach(funcBtn => {
+    funcBtn.addEventListener('click', (event) => {
+        operator = event.target.value;
+        if (primaryNumber) {
+            storedNumber = parseFloat(primaryNumber);
+            primaryNumber = "";
+        }
+    })
+})
+
+
 
 
